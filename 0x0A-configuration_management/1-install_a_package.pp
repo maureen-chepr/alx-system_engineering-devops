a0168c3ab06602e704439824f740cf0a76bb1978
maureen-chepr <maureenchep95@gmail.com>
2024-01-19 19:04:25 +0300

    inst flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # using puppet to install flask
 
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   path    => ['/usr/bin'],
-  require => Package['python3-pip'],
 }
