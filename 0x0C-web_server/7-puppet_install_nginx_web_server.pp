c7e01d138d712f6381bafa45852907fe8c024eee
root <root@Moh.localdomain>
2024-01-24 13:25:59 +0300

    install nginx w/puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,35 @@
+# Install Nginx web server (w/ Puppet)
+
+exec {'apt-get-update':
+  command => '/usr/bin/apt-get update'
+}
+
+package {'apache2.2-common':
+  ensure  => 'absent',
+  require => Exec['apt-get-update']
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Package['apache2.2-common']
+}
+
+service {'nginx':
+  ensure  =>  'running',
+  require => file_line['perform a redirection'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  require =>  Package['nginx']
+}
+
+file_line { 'perform a redirection':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => 'rewrite ^/redirect_me/$ https://github.com/maureen-chepr permanent;',
+  after   => 'root /var/www/html;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
