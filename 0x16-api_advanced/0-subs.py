2b99f4abdba264ecb6f5af83c7ee53837fa7146e
root <maureenchep95@gmail.com>
2024-03-05 22:32:15 +0300

    check data

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     function that queries the Reddit API and
     returns the number of subscribers
 """
@@ -8,10 +8,23 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    user = {"User-Agent": "maureen-chepr"}
-    request = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit), headers=user)
-    if request.status_code == 200:
-        return request.json().get("data").get("subscribers")
-    else:
+    """
+    function that queries the Reddit API and
+    returns the number of subscribers
+    """
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {'User-Agent': 'maureen-chepr'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json().get("data")
+        if data:
+            return data.get("subscribers", 0)
+        else:
+            return 0
+    except json.JSONDecodeError:
         return 0
