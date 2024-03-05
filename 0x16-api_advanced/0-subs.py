60f10638c864d14fb7f2a994ec59e5cf0da34d1c
root <maureenchep95@gmail.com>
2024-03-05 22:11:31 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 
+    function that queries the Reddit API and
+    returns the number of subscribers
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    user = {"User-Agent": "maureen-chepr"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
+        return 0
