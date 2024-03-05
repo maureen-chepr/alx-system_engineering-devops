c96fc75fb447b183ff9affac245468ce6a73dd91
root <maureenchep95@gmail.com>
2024-03-05 23:03:00 +0300

    task 1 try

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
+"""Function that querries subscribers on a given Reddit subreddit"""
+import json
 import requests
-"""
-functions that queries the reddit api
-"""
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and
-    prints the titles of the first 10 hot posts
-    """
-    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
-    header = {'User-Agent': 'maureen-chepr/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status_code != 200:
-        print(None)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "CustomClient/1.0 (by /u/Mundane_Strategy7626)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-    req = req.json()
-    if "data" in req:
-        for post in req.get("data").get("children"):
-            print(post.get("data").get("title"))
-    else:
-        print(None)
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
